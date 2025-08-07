2409304b24eb455dadb135966eea6ca9a75b9ba1
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-08-07 16:27:18 -0300

    add objeto de teste em arquivo separado

---
+++ db.js
@@ -0,0 +1,18 @@
+export let myServers = [
+    {
+      ip: "192.168.0.15",
+      machine: "celular",
+    },
+    {
+      ip: "192.168.0.20",
+      machine: "notebook",
+    },
+    {
+      ip: "192.168.0.27",
+      machine: "servidor",
+    },
+    {
+      ip: "192.168.0.37",
+      machine: "raspberry pi",
+    },
+  ];
