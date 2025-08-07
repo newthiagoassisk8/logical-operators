0d05d5a863a88a5b131d4f98c37ac0961c86d27c
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-08-07 16:26:19 -0300

    add: for of for each dowhile

--- exercises/checkServers.js
+++ exercises/checkServers.js
@@ -1,40 +1,66 @@
 import { checkServerAvailability } from "../config.js";
-
+import { myServers } from "../db.js";
+// Exemplo com for
 export async function checkServers(listServers) {
   for (let i = 0; i < listServers.length; i++) {
-    let asd = await checkServerAvailability(listServers[i].ip);
-    let statusServer = checkServerStatus(asd)
+    let isTheServerUp = await checkServerAvailability(listServers[i].ip);
+    let statusServer = checkServerStatus(isTheServerUp);
     listServers[i]["status"] = statusServer;
   }
   console.log(listServers);
   return listServers;
 }
+// Exemplo com for of
+export async function checkServersWithForOF(listServers) {
+  for (let server of listServers) {
+    let isTheServerUp = await checkServerAvailability(server.ip);
+    let statusServer = checkServerStatus(isTheServerUp);
 
-let myServers = [
-  {
-    ip: "192.168.0.15",
-    machine: "celular",
-  },
-  {
-    ip: "192.168.0.20",
-    machine: "notebook",
-  },
-  {
-    ip: "192.168.0.27",
-    machine: "servidor",
-  },
-  {
-    ip: "192.168.0.37",
-    machine: "raspberry pi",
-  },
-];
+    server["status"] = statusServer;
+  }
+  console.log(listServers);
 
-function checkServerStatus(isServerUp){
-    if(isServerUp){
-        return 'Up'
-    }
-    return 'Down'
+  return listServers;
+}
+
+// Exemplo com for each
+export async function checkServersWithForEach(listServers) {
+
+  listServers.forEach(async (el) => {
+    let isTheServerUp = await checkServerAvailability(el.ip);
+    let statusServer = checkServerStatus(isTheServerUp);
+    el["status"] = statusServer;
+  });
+  console.log(listServers);
+  return listServers;
+}
+
+// Exemplo com do while
+export async function checkServersWithDoWhile(listServers) {
+  let i = 0;
+  do {
+    let isTheServerUp = await checkServerAvailability(listServers[i].ip);
+    let statusServer = checkServerStatus(isTheServerUp);
+    listServers[i]["status"] = statusServer;
+    i += 1;
+  } while (i < listServers.length);
+  console.log(listServers);
+  return listServers;
+}
+
+// TODO: Aplicar continue
+// Exemplo com continue
+export async function checkServersWithDoWhile(listServers) {
 
 }
 
-await checkServers(myServers);
+function checkServerStatus(isServerUp) {
+  if (isServerUp) {
+    return "Up";
+  }
+  return "Down";
+}
+
+// await checkServers(myServers);
+// await checkServersWithForOF(myServers);
+await checkServersWithDoWhile(myServers);
